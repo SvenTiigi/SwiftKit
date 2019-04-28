93d17b661e5d6ea4d8de3327ca19c0d47de13add
Sven Tiigi <sven.tiigi@gmail.com>
2019-04-28 13:25:33 +0200

    Added executable product

--- Package.swift
+++ Package.swift
@@ -4,6 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftKit",
+    products: [
+        .executable(
+            name: "swiftkit",
+            targets: [
+                "SwiftKit"
+            ]
+        ),
+    ],
     dependencies: [
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", .exact("5.2.2"))
     ],
