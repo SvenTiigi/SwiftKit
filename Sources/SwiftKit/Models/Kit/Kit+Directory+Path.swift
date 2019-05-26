c5e831f982898db8e45b84d2be8d7ed9f3e54bb1
Sven Tiigi <sven.tiigi@gmail.com>
2019-05-26 13:42:41 +0200

    Added Documentation

--- Sources/SwiftKit/Models/Kit/Kit+Directory+Path.swift
+++ Sources/SwiftKit/Models/Kit/Kit+Directory+Path.swift
@@ -27,9 +27,12 @@ extension Kit.Directory.Path {
     
     /// The last path component
     var lastComponent: String? {
+        // Check if raw value can be constructed to an URL
         if let url = URL(string: self.rawValue) {
+            // Return last path component
             return url.lastPathComponent
         } else {
+            // Otherwise return last component separated by slash
             return self.rawValue
                 .drop(suffix: "/")
                 .components(separatedBy: "/")
@@ -47,18 +50,26 @@ extension Kit.Directory.Path {
     ///
     /// - Parameter pathComponent: The path component that should be appended
     mutating func append(_ pathComponent: String) {
+        // Check if raw value can be constructed to an URL
         if var url = URL(string: self.rawValue) {
+            // Append path component
             url.appendPathComponent(pathComponent)
+            // Set raw value
             self.rawValue = url.absoluteString
         } else {
+            // Initialize mutable copy of raw value
             var rawValue = self.rawValue
+            // Drop last slash character
             while rawValue.last == "/" {
                 rawValue = .init(rawValue.dropLast())
             }
+            // Initialize mutable path component
             var pathComponent = pathComponent
+            // Drop first slash character
             while pathComponent.first == "/" {
                 pathComponent = .init(pathComponent.dropFirst())
             }
+            // Concat raw value with path component
             self.rawValue = rawValue + "/" + pathComponent
         }
     }
@@ -68,8 +79,11 @@ extension Kit.Directory.Path {
     /// - Parameter pathComponent: The path component that should be appended
     /// - Returns: The new Path
     func appending(_ pathComponent: String) -> Kit.Directory.Path {
+        // Initialize mutable Path
         var path = self
+        // Append path component
         path.append(pathComponent)
+        // Return appended Path
         return path
     }
     
