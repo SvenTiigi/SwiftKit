7c5caa26e97efa6859d95dc78bc18e5cfa3963ff
Sven Tiigi <sven.tiigi@gmail.com>
2020-12-06 10:55:47 +0100

    Updated Platforms

--- Template/Package.swift
+++ Template/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "KITPROJECT",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
         .tvOS(.v9),
         .watchOS(.v2),
         .macOS(.v10_10)
